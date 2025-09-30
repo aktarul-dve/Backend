d6669a64921e9b4437a3ff694d75b5337c28267f
aktarul-dve <islammdaktarul345@gmail.com>
2025-09-30 09:27:24 +0600

    Update UserActionRoute.js

--- routers/UserActionRoute.js
+++ routers/UserActionRoute.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const User = require("../models/User");
 
+// Action hit
 router.post("/count", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
 
-    // Increase actionCount
     user.actionCount += 1;
-
     let rewardTriggered = false;
 
     if (user.actionCount >= 10) {
@@ -20,17 +19,21 @@ router.post("/count", auth, async (req, res) => {
 
     await user.save();
 
-    // 🔹 Emit via Socket.io
+    // Emit to user-specific room
     const io = req.app.get("io");
-    io.emit("user_update", {
+    io.to(user._id.toString()).emit("user_update", {
       userId: user._id.toString(),
-      updatedFields: { actionCount: user.actionCount, }
+      updatedFields: {
+        actionCount: user.actionCount,
+       
+      }
     });
 
     res.json({
       success: true,
       rewardTriggered,
-      actionCount: user.actionCount
+      actionCount: user.actionCount,
+     
     });
 
   } catch (err) {
