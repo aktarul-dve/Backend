6745543efcdd370bb2074c8141fe5a092cf4a0c6
aktarul-dve <islammdaktarul345@gmail.com>
2025-09-30 18:17:54 +0600

    Update ArticleModel.js

--- models/ArticleModel.js
+++ models/ArticleModel.js
@@ -5,7 +5,36 @@ const ArticleModel = new mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
   
+});// models/Question.js
+const mongoose = require("mongoose");
+
+const ArticleModel = new mongoose.Schema({
+  Title: { 
+      type: String,
+     required: true 
+    },
+  Description: { 
+    type: String, 
+    required: true
+   },
+    photo:{
+       public_id :{
+        type:String,
+        required:true,
+       },
+       url:{
+        type:String,
+        required:true,
+
+       }
+    }
+  
+  
 });
+
+const Article = mongoose.model("Article", ArticleModel);
+module.exports = Article;
+
 
 const Article = mongoose.model("Article", ArticleModel);
 module.exports = Article;
